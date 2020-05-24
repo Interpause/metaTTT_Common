6b7381823f9fa19b07280754fdf2a03529af4a56
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2020-05-24 12:05:52 +0800

    global utils yayay

---
+++ utils/utils.js
@@ -0,0 +1,4 @@
+if(window) g = window;
+if(global) g = global;
+g.gen_uuid = c => ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+g.wait = ms => new Promise((r, j)=>setTimeout(r, ms));
